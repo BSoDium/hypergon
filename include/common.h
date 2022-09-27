f0fb3583197e75a44e88e5718d564e6fd00fbf54
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2022-09-27 04:24:30 +0200

    build: rewrite cmake configuration

---
+++ include/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+namespace hypergon
+{
+  void hello();
+}
+
+#endif // COMMON_HPP
---
+++ src/common.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace hypergon
+{
+  void hello()
+  {
+    std::cout << "Hello, world!" << std::endl;
+  }
+}
